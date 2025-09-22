e55e17978a78179bdc869a39b7f009fa3d288efa
DiegoRomero <diego.rcorvera@gmail.com>
2025-09-22 10:47:52 -0600

    Cambios completo de reacomodo estilo Orientado Objetos, creando dos clases: Entrada y Bitacora con sus respectivos metodos

---
+++ Bitacora.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cctype>
+#include <string>
+
+#include "Entrada.h"
+
+using namespace std;
+
+class Bitacora {
+    public:
+        vector<Entrada> vect;
+
+        Bitacora();
+
+        void leerArchivo(string nombre);
+        void crearArchivo(string nombre);
+
+        void leerBitacora();
+
+        //
+        void mezcla(int ini, int mid, int fin);
+
+        //
+        void mergeRecursivo(int ini, int fin);
+
+        //
+        void mergeSort();
+
+};
